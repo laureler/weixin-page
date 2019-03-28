5dd898c8915e16496b724397efa5f705bad3bebb
zhengyekun <1634372267@qq.com>
2019-03-28 18:04:42 +0800

    vue.config.js配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,43 @@
 // vue.config.ts 配置说明
 
+const path = require('path')
+
+function resolve (dir) {
+    return path.join(__dirname, dir)
+}
+
 module.exports = {
 
-  publicPath: '/',
+    publicPath: '/',
 
-  // 打包后输出路径
-  outputDir: 'dist',
+    // 打包后输出路径
+    outputDir: 'dist',
 
-  // 保存时是不是用esLint-loader 来lint 代码
-  lintOnSave: true,
+    // 保存时是不是用esLint-loader 来lint 代码
+    lintOnSave: true,
 
-  devServer: { // 配置服务器
-    host: '0.0.0.0',
-    port: 3000, // 端口号
-    https: false,
-    compress: true,
-    open: true, // 配置自动启动浏览器
-    hotOnly: true
-  },
+    devServer: { // 配置服务器
+        host: '0.0.0.0',
+        port: 3000, // 端口号
+        https: false,
+        compress: true,
+        open: true, // 配置自动启动浏览器
+        hotOnly: true
+    },
 
-  pluginOptions: {
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            // 为生产环境修改配置...
+            config.plugins.push(
+                new BundleAnalyzerPlugin()
+            );
 
-  }
+        } else {
+            // 为开发环境修改配置...
+        }
+
+    },
+
+    pluginOptions: {}
 
 }
