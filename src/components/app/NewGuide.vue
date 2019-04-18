2652531b8c388a06f975df75280370f85eba92e0
鄢新疆 <2053334706@qq.com>
2019-04-18 14:54:22 +0800

    提交微信公众号的办事指南

---
+++ src/components/app/NewGuide.vue
@@ -0,0 +1,199 @@
+<template lang="html">
+	<div style="display:flex;flex-direction:column;background-color:#F0F5F8">
+		<page-head title="办事指南"></page-head>
+        <div class="search-div">
+            <div class="s-div">
+                <input v-model="searchData" type="text" class="s-input"/>
+                <el-button v-on:click="query" class="s-query" icon="el-icon-search"></el-button>
+            </div>
+        </div>
+		<div class="contain">
+			<van-cell-group>
+				<cell-line @click.native="open(index)" v-for="(item,index) in datas" :title="item.TITLE"/>
+			</van-cell-group>
+		</div>
+	</div>
+</template>
+
+<script>
+
+    import Head from './head.vue'
+    import { request } from '../../utils/http'
+    import cellLine from './cellLine.vue'
+
+    export default {
+        components: {
+            'page-head': Head,
+            'cell-line': cellLine
+        },
+        data () {
+            return {
+                //当前页码
+                currentPage: 1,
+                items: [],
+                // 全部数据
+                datas: [],
+                //标题名称
+                pageSize: 10,
+                totalDataNumber: 0,
+                //查询条件
+                searchData:''
+            }
+        },
+        // 计算属性
+        computed: {
+            calcItems: function () {
+                var _this = this
+                return _this.datas.slice(_this.pageSize * (_this.currentPage - 1), _this.pageSize * _this.currentPage)
+            }
+        },
+        methods: {
+            query () {
+                const that = this
+                request({
+                    url: '/pubWeb/public/doIntranetRequest/GetTitleList',
+                    data:{strJson:JSON.stringify({filter:decodeURI(that.searchData),bname:decodeURI(that.$route.query.response)})},
+                    success (data) {
+                        that.datas = data.noInfo;
+                    },
+                    fail (error) {
+                        if(error.status == '404'){
+                            alert("找不到该接口！");
+                            return;
+                        }
+                    },
+                })
+            },
+            open (index) {
+                var url =  this.datas[index].APP_URL;
+                window.location.href = url;
+            },
+            loadData (pageNum, pageSize) {
+                const that = this
+                that.items = []
+                for (let i = 0; i < that.datas.length; i++) {
+                    if (i >= (parseInt(pageNum) - 1) * pageSize && i < parseInt(pageNum) * parseInt(pageSize)) {
+                        that.items.push(that.datas[i])
+                    }
+                }
+            },
+            handleCurrentChange (val) {
+                const that = this
+                that.currentPage = val
+                that.loadData(val, that.pageSize)
+                that.$store.commit('changeCurrent', {
+                    currentPage: val,
+                })
+            },
+            record () {
+                const that = this
+                this.$store.commit(mutationTypes.GET_RECORD, {
+                    datas: that.datas,
+                    bname: that.bname,
+                })
+                that.$store.commit('changeState', {
+                    bname: that.bname,
+                    datas: that.datas,
+                })
+                that.$store.commit('changeCurrent', {
+                    currentPage: that.currentPage,
+                })
+            },
+        },
+        mounted () {
+            const that = this
+            if (that.$store.state.bname == that.$route.query.response && that.$store.state.datas.length > 0 && !that.$route.params.id) {
+                const num = that.$store.state.currentPage
+                that.datas = that.$store.state.datas
+                that.totalDataNumber = that.datas.length
+                that.bname = that.$store.state.bname
+                if (num) {
+                    that.currentPage = num
+                }
+                for (let i = 0; i < that.datas.length; i++) {
+                    if (i >= (that.currentPage - 1) * that.pageSize && i < that.currentPage * that.pageSize) {
+                        that.items.push(that.datas[i])
+                    }
+                }
+            } else {
+                request({
+                    url: '/pubWeb/public/doIntranetRequest/GetTitleList',
+                    //data: { strJson: JSON.stringify({ bname: decodeURI(that.$route.query.response) }) },
+                    success (data) {
+                        that.datas = data.noInfo;
+                    },
+                    fail (error) {
+                        if(error.status == '404'){
+                            alert("找不到该接口！");
+                            return;
+                        }
+                    },
+                })
+            }
+        },
+    }
+</script>
+
+<style lang="css" scoped>
+
+	@import "../../css/elem UI.css";
+
+	.van-cell__title {
+		width: 100% !important;
+	}
+
+	.contain {
+		background-color: #ffffff;
+		margin-top: 1.95rem;
+	}
+
+	.paging {
+		padding: 0.15rem;
+	}
+
+    .search-div {
+		background: #f0f5f8;
+        width: 100%;
+		height: 2rem;
+        position: fixed;
+        top: 1.2rem;
+        left: 0;
+        z-index: 2001;
+	}
+
+	.s-query {
+		margin: 0 auto;
+		border: none;
+		color: #6cccff;
+		margin-top: -0.1rem;
+		padding-left: 0.1rem;
+		height: 1rem;
+	}
+
+	.s-query:focus {
+		background: white;
+	}
+
+	.s-query:hover {
+		background: white;
+	}
+
+	.s-div {
+		padding: 0.3rem 0rem 0.3rem 0.3rem;
+		display: flex;
+		font-size: 0.375rem;
+		margin: 0.25rem 0.25rem 0.25rem 0.25rem;
+		background-color: #ffffff;
+		height: 1.5rem;
+	}
+
+	.s-input {
+		width: 85%;
+		border: none;
+		font-size: 0.375rem;
+		outline: none;
+		border-right: 1px solid #e5e5e5;
+		height: 1rem;
+		margin-right: 0.31rem;
+	}
+</style>
