83f900fa204730189f0d70e1b2abb3be59db5cdd
yunlong <laureler@163.com>
2019-08-07 14:36:23 +0800

    9208	1-最高	（热更新）中山-微信端证书查验功能

--- src/page/certificateCheck.vue
+++ src/page/certificateCheck.vue
@@ -1,35 +1,42 @@
 <template>
 	<div id="certificateCheck" style="height: 100%;width: 100%">
-		<van-tabs v-model="active">
+
+		<van-tabs v-model="active" @change="changeTab">
 			<van-tab title="证书查验">
 				<van-cell-group>
-					<van-field label="权利人" v-model="quanliren" placeholder="请填写权利人" />
-					<van-cell title="证件类型" is-link :value="cardType" @click="selectType"/>
-					<van-field label="证件号码" v-model="cardNumber"  placeholder="请填写证件号码" />
-
-					<van-cell value="证书号" />
+					<van-cell value="权利人"/>
+					<van-field class="lineBox" v-model="quanliren" placeholder="请填写权利人"/>
+					<van-cell value="证件类型"/>
+					<van-cell class="lineBox" is-link :value="cardType" @click="selectType" :class="selectedValue"/>
+					<van-cell value="证件号码"/>
+					<van-field class="lineBox" v-model="cardNumber" placeholder="请填写证件号码"/>
+					<van-cell value="证书号"/>
 					<div style="display: flex;flex-direction: row;margin: 2px 14px; font-size: 16px;color: #989898">
 						<div style="transform: translateY(0%)">
 							<div @click="selectYear" style="display: inline">
 								<span style="margin-right: 10px;">粤</span>
-								<input type="text" v-model="year" style="border: 1px solid #989898;width: 78px;height: 40px;text-align: center">
+								<input type="text" v-model="year" disabled
+								       style="border: 1px solid #989898;width: 78px;height: 40px;text-align: left;background: white;padding-left: 5px !important">
+								<i data-v-03a2a6a8="" style="margin-left: -20px" class="van-icon van-icon-arrow van-cell__right-icon"><!----></i>
 							</div>
-							<span style="margin: 0 10px;">中山市不第产权第</span>
-							<input type="text" v-model="budongchanquanzheng_number" style="border: 1px solid #989898;width: 78px;height: 40px;text-align: center">
+							<span style="margin: 0 6px;">中山市不动产权第</span>
+							<input type="text" v-model="budongchanquanzheng_number"
+							       style="border: 1px solid #989898;width: 78px;height: 40px;text-align: left;padding-left: 5px !important;">
 							<span style="margin: 0 10px;">号</span>
 						</div>
 					</div>
-<!--					<van-field label="验证码"  placeholder="请输入验证码" />-->
-<!--					<van-col span="12">-->
-<!--						<img src="../../public/images/wechatstatic/bg_top.png" class="img"/>-->
-<!--					</van-col>-->
-					<van-cell value="" />
-					<div style="color: #989898;">
+					<!--					<van-field class="lineBox" label="验证码"  placeholder="请输入验证码" />-->
+					<!--					<van-col span="12">-->
+					<!--						<img src="../../public/images/wechatstatic/bg_top.png" class="img"/>-->
+					<!--					</van-col>-->
+<!--					<van-cell value=""/>-->
+					<!--<div style="color: #989898;">
 						<div style="text-align: center">温馨提示：本网站只提供本级管辖区域内的不动产信息查询</div>
-					</div>
-					<div style="display: flex;flex-direction: row;justify-content: center;width: 80%;margin: 0 auto" class="buttonArea">
-						<van-button type="info" @click="find_zhengshu">查询</van-button>
-						<van-button type="default">重置</van-button>
+					</div>-->
+					<div style="display: flex;flex-direction: row;justify-content: center;width: 80%;margin: 0 auto"
+					     class="buttonArea">
+						<van-button :loading="loading" type="info" @click="find_zhengshu">查询</van-button>
+						<van-button type="default" @click="resetValue">重置</van-button>
 					</div>
 
 					<div class="successDiv" v-show="successDiv">
@@ -38,24 +45,84 @@
 							<span class="successSpan">检验通过</span>
 						</div>
 					</div>
-					<div class="successDiv" v-show="failDiv">
+					<div class="failDiv" v-show="failDiv">
 						<div style="transform: translateY(50%)">
 							<img src="../../public/images/不存在@2x.png" class="successImg"/>
-							<span class="successSpan">检验通过</span>
+							<span class="failSpan">不存在该产权证书，请确认输入信息是否准确</span>
 						</div>
 					</div>
 				</van-cell-group>
+				<van-popup v-model="show" position="bottom" :overlay="false">
+					<van-picker show-toolbar :columns="cardTypes" @confirm="confirmPicker" @change="onChange"
+					            @cancel="show = false"/>
+				</van-popup>
+				<van-popup v-model="yearSelect" position="bottom" :overlay="false">
+					<van-picker show-toolbar :columns="years" @confirm="confirmPickerYear" @change="onChange"
+					            @cancel="yearSelect = false"/>
+				</van-popup>
 			</van-tab>
 			<van-tab title="证明查验">
+				<van-cell-group>
+					<van-cell value="权利人"/>
+					<van-field class="lineBox" v-model="quanliren" placeholder="请填写权利人"/>
+					<van-cell value="证件类型"/>
+					<van-cell class="lineBox" is-link :value="cardType" @click="selectType" :class="selectedValue"/>
+					<van-cell value="证件号码"/>
+					<van-field class="lineBox" v-model="cardNumber" placeholder="请填写证件号码"/>
+					<van-cell value="证明号"/>
+					<div style="display: flex;flex-direction: row;margin: 2px 14px; font-size: 16px;color: #989898">
+						<div style="transform: translateY(0%)">
+							<div @click="selectYear" style="display: inline">
+								<span style="margin-right: 2px;">粤</span>
+								<input type="text" v-model="year"
+								       style="border: 1px solid #989898;width: 78px;height: 40px;text-align: left;background: white;padding-left: 5px !important">
+								<i data-v-03a2a6a8="" style="margin-left: -20px" class="van-icon van-icon-arrow van-cell__right-icon"><!----></i>
+							</div>
+							<span style="margin: 0 4px;">中山市不动产证明第</span>
+							<input type="text" v-model="budongchanquanzheng_number"
+							       style="border: 1px solid #989898;width: 78px;height: 40px;text-align: center">
+							<span style="margin: 0 2px;">号</span>
+						</div>
+					</div>
+					<!--					<van-field class="lineBox" label="验证码"  placeholder="请输入验证码" />-->
+					<!--					<van-col span="12">-->
+					<!--						<img src="../../public/images/wechatstatic/bg_top.png" class="img"/>-->
+					<!--					</van-col>-->
+<!--					<van-cell value=""/>-->
+					<!--<div style="color: #989898;">
+						<div style="text-align: center">温馨提示：本网站只提供本级管辖区域内的不动产信息查询</div>
+					</div>-->
+					<div style="display: flex;flex-direction: row;justify-content: center;width: 80%;margin: 0 auto"
+					     class="buttonArea">
+						<!--<van-loading />-->
+						<van-button :loading="loading" type="info" @click="find_zhengshu">查询</van-button>
+						<van-button type="default" @click="resetValue">重置</van-button>
+					</div>
 
+					<div class="successDiv" v-show="successDiv">
+						<div style="transform: translateY(50%)">
+							<img src="../../public/images/通过@2x.png" class="successImg"/>
+							<span class="successSpan">检验通过</span>
+						</div>
+					</div>
+					<div class="failDiv" v-show="failDiv">
+						<div style="transform: translateY(50%)">
+							<img src="../../public/images/不存在@2x.png" class="successImg"/>
+							<span class="failSpan">不存在该产权证书，请确认输入信息是否准确</span>
+						</div>
+					</div>
+				</van-cell-group>
+				<van-popup v-model="show" position="bottom" :overlay="false">
+					<van-picker show-toolbar :columns="cardTypes" @confirm="confirmPicker" @change="onChange"
+					            @cancel="show = false"/>
+				</van-popup>
+				<van-popup v-model="yearSelect" position="bottom" :overlay="false">
+					<van-picker show-toolbar :columns="years" @confirm="confirmPickerYear" @change="onChange"
+					            @cancel="yearSelect = false"/>
+				</van-popup>
 			</van-tab>
 		</van-tabs>
-		<van-popup v-model="show" position="bottom" :overlay="false">
-			<van-picker show-toolbar :columns="cardTypes" @confirm="confirmPicker" @change="onChange" @cancel="show = false"/>
-		</van-popup>
-		<van-popup v-model="yearSelect" position="bottom" :overlay="false">
-			<van-picker show-toolbar :columns="years" @confirm="confirmPickerYear" @change="onChange" @cancel="show = false"/>
-		</van-popup>
+
 	</div>
 </template>
 
@@ -72,17 +139,19 @@
 				yearSelect: false,
 				show: false,
 				successDiv: false,
-				cardTypes: ['身份证', '港澳台身份证', '护照', '户口簿', '军官证（士兵证）'],
+				loading: false,
+				failDiv: false,
+				cardTypes: ['身份证', '港澳台身份证', '护照', '户口簿', '军官证（士兵证）', '组织机构代码', '营业执照', '其他'],
 				year: '',
 				budongchanquanzheng_number: '',
-				years: ['2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026', '2027', '2028', '2029', '2030', '2031', '2032', '2033', '2034', '2035', '2036', ],
+				years: ['2015', '2016', '2017', '2018', '2019'],
 				// 权利人
 				quanliren: "",
 				// 当前证件类型
-				cardType: "",
+				cardType: "请选择证件类型",
 				cardTypeNumber: "",
 				cardNumber: "",
-				active: 2,
+				active: 0,
 				actions: [
 					{
 						name: '选项'
@@ -101,22 +170,54 @@
 				]
 			}
 		},
-		computed: {},
-		watch: {},
+		computed: {
+			selectedValue: function () {
+				if (this.cardType !== '请选择证件类型' && this.cardType !== null) {
+					return 'selectedValue';
+				} else {
+					return 'unselectedValue';
+				}
+			}
+		},
+		watch: {
+			active: function (oldval, newval) {
+				this.resetValue();
+			}
+		},
 		methods: {
+			changeTab () {
+				this.yearSelect = false;
+				this.show = false;
+			},
+			resetValue () {
+				this.quanliren = '';
+				this.cardType = '请选择证件类型';
+				this.cardTypeNumber = '';
+				this.cardNumber = '';
+				this.year = '';
+				this.budongchanquanzheng_number = '';
+				this.successDiv = false;
+				this.failDiv = false;
+			},
 			selectYear () {
+				this.show = false;
 				this.yearSelect = true;
 			},
 			find_zhengshu () {
+				if (this.loading === true) {
+					return;
+				}
+				this.successDiv = false;
+				this.failDiv = false;
 				if (this.quanliren === null || this.quanliren === '') {
 					Toast('请填写权利人');
 					return;
 				}
-				if (this.cardType === null || this.cardType === '') {
+				if (this.cardType === null || this.cardType === '请选择证件类型') {
 					Toast('请选择证件类型');
 					return;
 				}
-				if (this.cardNumber === null || this.cardNumber === '') {
+				if (this.cardNumber === null || this.cardNumber === '' || this.cardType == null || this.cardType == '请选择证件类型') {
 					Toast('请填写证件号码');
 					return;
 				}
@@ -128,22 +229,95 @@
 					Toast('请填写不动产权证号2');
 					return;
 				}
-				let params = {
+
+				/* if (!(/^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/.test(this.phoNumber))) {
+					Toast('手机号码格式不正确！')
+				} */
+				switch (this.cardType) {
+					case "组织机构代码":
+						// 验证组织机构代码 或 营业执照 是否符合格式
+						if (!(/^([0-9a-zA-Z]{18}$|\d{15}$)/.test(this.cardNumber))) {
+							Toast('组织机构代码或营业执照格式不正确！')
+							return;
+						}
+						break;
+					case "营业执照":
+						// 验证组织机构代码 或 营业执照 是否符合格式
+						if (!(/^([0-9a-zA-Z]{18}$|\d{15}$)/.test(this.cardNumber))) {
+							Toast('组织机构代码或营业执照格式不正确！')
+							return;
+						}
+						break;
+					case "其他":
+						break;
+					case "身份证":
+						if (!(/(^\d{15}$)|(^\d{17}([0-9]|X)$)/.test(this.cardNumber))) {
+							Toast('身份证格式不正确！')
+							return;
+						}
+						break;
+					case "护照":
+						if (!(/^[a-zA-Z0-9]{3,21}$/.test(this.cardNumber)) || !(/^(P\d{7})|(G\d{8})$/.test(this.cardNumber))) {
+							Toast('护照号码格式不正确！')
+							return;
+						}
+						break;
+					case "港澳台身份证":
+						const aomenreg = /^[157][0-9]{6}\([0-9]\)$/;	// 补充澳门身份证正则
+						const taiwanreg = /^\d{8}|^[a-zA-Z0-9]{10}|^\d{18}$/;	// 台湾身份证正则
+						const GAReg = /^([A-Z]\d{6,10}(\(\w{1}\))?)$/;	// 港澳身份证正则
+						if (taiwanreg.test(this.cardNumber) || GAReg.test(this.cardNumber) || aomenreg.test(this.cardNumber)) {} else {
+							Toast('港澳台身份证号码格式不正确！');
+							return;
+						}
+						break;
+					case "户口簿":
+						if (!(/^[a-zA-Z0-9]{3,21}$/.test(this.cardNumber))) {
+							Toast('户口簿号码格式不正确！')
+							return;
+						}
+						break;
+				}
+				// 准备网络请求
+				let strJson = {
+					"username": "nfdj",
+					"password": "nfdj1234",
 					qlr: this.quanliren, // 权利人
 					zjzl: this.cardTypeNumber, // 证件种类
 					zjhm: this.cardNumber, // 证件号码
 					cqzh: `粤 （${this.year}） 中山市 不动产权第 ${this.budongchanquanzheng_number} 号`, // 不动产权证号/不动产登记证明号
-					zlx: "", // 正类型(1 不动产权证）（2 不动产登记证明）
+					zlx: this.active + 1, // 正类型(1 不动产权证）（2 不动产登记证明）
 				}
+				if (this.active == 1) {
+					strJson.cqzh = `粤 （${this.year}） 中山市 不动产证明第 ${this.budongchanquanzheng_number} 号`; // 不动产权证号/不动产登记证明号
+				}
+				// 最后一位 是其他
+				if (this.cardTypeNumber == 8) {
+					strJson.zjhm = 8;
+				}
+				/* strJson = {
+					"username": "nfdj",
+					"password": "nfdj1234",
+					qlr: '郑潮平', // 权利人
+					zjzl: 1, // 证件种类
+					zjhm: '440582199108241557', // 证件号码
+					cqzh: `粤（2017）中山市不动产权第 0251548 号`, // 不动产权证号/不动产登记证明号
+					zlx: "1", // 正类型(1 不动产权证）（2 不动产登记证明）
+				} */
+				// var formData = new FormData();
+				// formData.append("parentId",parsedItem.id)
 				var that = this;
+				this.loading = true;
 				request({
-					// http://bdcdj.zsfdc.gov.cn/WSYY/CheckCert?strJson={"username":"nfdj","password":"nfdj1234","qlr":"郑潮平","zjzl":"1","zjhm":"440582199108241557","cqzh":"粤（2017）中山市不动产权第0251548号","zlx":"1"}
+					// http://bdcdj.zsfdc.gov.cn/WSYY/CheckCert?strJson={"username":"nfdj","password":"nfdj1234","qlr":"郑潮平","zjzl":"1","zjhm":"440582199108241557","cqzh":"粤（2017）中山市不动产权第 0251548 号","zlx":"1"}
 					url: '/CheckCert',
-					data: { strJson: JSON.stringify(params) },
+					data: { strJson: JSON.stringify(strJson) },
 					success (response) {
+						that.loading = false;
 						if (response.resultcode == 0 || response.resultcode == "0") {
-							Toast('不存在该产权证书，请确认输入信息是否准确');
-							console.log('返回结果为0' + result.resultcode);
+							// Toast('不存在该产权证书，请确认输入信息是否准确');
+							that.failDiv = true;
+							console.log('返回结果为0' + response.resultcode);
 						} else if (response.resultcode == 0 || response.resultcode == "0") {
 							if (response.result == "true" || response.resultcode == true) {
 								that.successDiv = true;
@@ -151,11 +325,13 @@
 						}
 					},
 					fail (error) {
+						that.loading = false;
 						Toast('不存在该产权证书，请确认输入信息是否准确');
-						console.log(error);
+						console.error(error);
+						console.error(error);
+						console.error(error);
 					},
 				});
-				// todo 查询
 			},
 			formatter (type, value) {
 				if (type === 'year') {
@@ -167,6 +343,7 @@
 			},
 			// 选择证件类型
 			selectType () {
+				this.yearSelect = false;
 				this.show = true;
 			},
 			confirmPickerYear (value, index) {
@@ -194,34 +371,62 @@
 </script>
 
 <style lang="less" scoped>
-	.img{
+	.img {
 		width: 30px;
 		height: 30px;
 	}
+
 	.successImg {
-		height: 14px;margin-top: -4px;margin-left: 10px
+		height: 14px;
+		margin-top: -4px;
+		margin-left: 10px
 	}
+
 	.successSpan {
-		font-size: 14px;height: 22px;color: #00c058;margin-left: 10px;
+		font-size: 14px;
+		height: 22px;
+		color: #00c058;
+		margin-left: 10px;
 	}
+
 	.successDiv {
-		background: #f0faec;height: 44px;
+		background: #f0faec;
+		height: 44px;
 	}
+
 	.failDiv {
-		background: #f0faec;height: 44px;
+		background: #ffe7e7;
+		height: 44px;
 	}
+
 	.buttonArea button {
 		margin: 20px;
 		width: 120px;
 		border-radius: 6px;
 	}
-	.van-cell__value >span {
+
+	.van-cell__value > span {
 		float: left;
 	}
-	.van-cell__value {
+	.unselectedValue  .van-cell__value--alone {
+		color: #969799;
+	}
+	.selectedValue {
 		color: black;
 	}
+
 	.van-cell:not(:last-child)::after {
 		border-bottom: 0px;
+	}
+	.failSpan {
+		font-size: 14px;
+		height: 22px;
+		margin-left: 10px;
+		color: #ff4064;
+	}
+	.lineBox {
+		border: 1px solid #d1d1d1;
+		width: auto;
+		margin: 0px 14px;
 	}
 </style>
