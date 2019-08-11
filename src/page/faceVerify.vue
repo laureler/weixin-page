aa7557fcd9f8d0ad17f5e41d773aadfcc3db8502
yunlong <laureler@163.com>
2019-08-11 18:19:22 +0800

    提交微信人脸核身的跳转设置

--- src/page/faceVerify.vue
+++ src/page/faceVerify.vue
@@ -7,6 +7,7 @@
 </template>
 
 <script>
+	import { Toast } from 'vant';
 	export default {
 		name: 'faceVerify',
 		props: {},
@@ -18,35 +19,74 @@
 				message: null, // 页面跳转返回信息
 				verifyCode: null, // 人脸识别返回码
 				checkCode: null, // 设备检查返回码
+				callbackUrl: null, // 回调地址
 				verify_result: null // 人脸核身认证返回凭证
 			}
 		},
 		computed: {},
 		watch: {},
 		methods: {
+			startVerify () {
+				debugger
+				console.log("准备开始人脸核身过程")
+				let checkParams = this.checkParams();
+				if (checkParams) {
+					console.log('checkParams正确，准备开始微信认证,paramsReuslt:' + checkParams);
+					this.loading = false;
+					console.log("开始微信认证")
+					this.checkVerifySupport();
+				} else {
+					console.log('checkParams不正确：' + checkParams);
+					console.log(this.personName);
+					console.log(this.personId);
+					console.log(this.callbackUrl);
+					Toast('参数不正确');
+				}
+			},
+			// 人脸识别认证结束
+			done (res) {
+				this.returnBack(res);
+			},
 			// 携带参数返回来源网址
 			returnBack () {
-				const urlParam = {
-					// 返回的消息
-					message: this.message,
-					// 检查设备是否验证 状态码
-					verifyCode: this.verifyCode,
-					// 检查设备是否支持 状态码
-					checkCode: this.checkCode
-				};
-				window.location = this.callbackUrl + urlParam.toString();
+				let message = this.message == null ? '' : this.message;
+				// 检查设备是否验证 状态码
+				let verifyCode = this.verifyCode == null ? '' : this.verifyCode;
+				// 检查设备是否支持 状态码
+				let checkCode = this.checkCode == null ? '' : this.checkCode;
+				window.location = this.callbackUrl + `?verifyCode=${verifyCode}&checkCode=${checkCode}&message=${message}`;
 			},
 			// 获取url地址参数
 			getUrlPara (name) {
-				var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+				var reg = new RegExp("(^|&|##)" + name + "=([^&]*)(&|$)", "i");
 				var r = window.location.search.substr(1).match(reg);
 				if (r != null) return (r[2]);
 				return null;
 			},
 			// 检查当前页面跳转的参数
 			checkParams () {
-				let personId = this.getUrlPara('personId');
-				let personName = this.getUrlPara('personName');
+				console.log("开始校验参数");
+				var hrefString = decodeURIComponent(window.location.href)
+				console.log("window.location.href:", hrefString);
+				// params=吴云龙##411122199202088250##www.baidu.com
+				var params = hrefString.split("?")[1].split("params=")[1];
+
+				console.log("params", params);
+				var paramsArray = params.split("##");
+				console.log(paramsArray);
+				let personName = paramsArray[0];
+				let personId = paramsArray[1];
+				// 最多兼容URL地址中含有一个 “##” 特殊字符
+				let callbackUrl = paramsArray.length > 3 ? paramsArray.concat("##", paramsArray[3]) : paramsArray[2];
+				console.log("准备开始校验");
+				console.log("personName", personName);
+				console.log("personId", personId);
+				console.log("callbackUrl", callbackUrl);
+				if (callbackUrl == null || callbackUrl == '') {
+					this.verifyCode = '';
+					this.message = '回调函数不存在或者回调函数为空';
+					return false;
+				}
 				if (personName == null || personName.length < 2) {
 					this.verifyCode = '';
 					this.message = '姓名不存在或者小于两位';
@@ -59,59 +99,66 @@
 					return false;
 				}
 				this.personName = personName;
+				this.callbackUrl = callbackUrl;
 				this.personId = personId;
 				return true;
 			},
 			checkVerifySupport () {
 				let _this = this;
+				let info = {
+					'request_verify_pre_info': '{"name":"' + _this.personName + '","id_card_number":"' + _this.personId + '"}',
+					'check_alive_type': '1'
+				};
 				// 人脸核身回调
 				let faceVerifyCallback = function (res) {
-					this.verifyCode = res.err_code;
-					if (res.err_code == 0) { // 0代表人脸识别成功
-						this.message = "人脸识别认证成功！";
-						this.verify_result = res.verify_result;
-						this.returnBack()
+					console.log('faceVerify result' + res.err_code)
+					_this.verifyCode = res.err_code;
+					if (res.hasOwnProperty('err_code')) {
+						_this.done(res);
 					} else {
-						this.message = this.err_msg;
-						this.returnBack();
-					}
-				};
-				// 人脸核身
-				let faceVericy = function () {
-					let info = {
-						request_verify_pre_info: '{"name":"' + _this.personName + '","id_card_number":"' + _this.personId + '"}',
-						check_alive_type: '2'
-					};
-					wx.invoke('requestWxFacePictureVerifyUnionVideo', info.toString(), faceVerifyCallback);
-				};
-				// 设备检测后回调
-				let checkSupportFaceDetectCallback = function (res) {
-					if (res.err_code == 0) { // 0代表检测成功
-						faceVericy()
-					} else {
-						this.checkCode = res.err_code;
-						this.message = '当前设备不支持微信人脸核身！';
-						this.returnBack();
+						wx.invoke('requestWxFacePictureVerify', info, function (res) {
+							_this.done(res);
+						});
 					}
 				};
 				// step1 检测设备是否支持人脸检测
-				wx.invoke('checkIsSupportFaceDetect', { "check_alive_type": "2" }, checkSupportFaceDetectCallback);
+				wx.invoke('checkIsSupportFaceDetect', {}, function (res) {
+					console.log("微信设备检查结束,结果为：" + JSON.stringify(res));
+					_this.checkCode = res.err_code;
+					if (res.err_code == 0) { // 0代表检测成功
+						console.log("检查结束，检查成功")
+						wx.invoke('requestWxFacePictureVerifyUnionVideo', info, faceVerifyCallback);
+					} else {
+						console.log("检查结束，检查失败");
+						console.log("res.err_code:" + res.err_code);
+						_this.message = '当前设备不支持微信人脸核身！';
+						_this.returnBack();
+					}
+				});
 			}
 		},
 		mounted: function () {
+			console.log("start mounted");
+			let _this = this;
 			let configPath = '/pubWeb/public/getWeChatConfig?url=' + window.location.href.split('#')[0];
+			console.log("configPath" + configPath);
+			// _this.startVerify();
 			this.$fetch(configPath)
 				.then(res => {
 				wx.config(res); // 注入权限验证配置
-				this.callbackUrl = this.getUrlPara('callbackUrl');
-				let checkParams = this.checkParams();
-				if (checkParams) {
-					this.loading = false;
-					this.checkVerifySupport()
-				}
+				wx.ready(function () {
+					_this.startVerify();
+				})
+				// this.callbackUrl = this.getUrlPara('callbackUrl');
+				// let checkParams = this.checkParams();
+				// if (checkParams) {
+				// 	this.loading = false;
+				// 	this.checkVerifySupport()
+				// }
 			})
 				.catch(error => {
 					console.error(error);
+					console.log(error);
 				})
 		},
 	}
